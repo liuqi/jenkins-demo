3d3cc52f2885d77b864e9b0ec7305d7af812bda7
zyajing <zyajing@vmware.com>
2022-02-23 01:42:34 -0800

    Update Tanzu cluster and kubeflow deploy files

---
+++ kubeflow-deployer.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+number=24
+ns=zyajing
+server=10.117.233.2
+
+export PATH=00-kubectl-vsphere-plugin/bin:$PATH
+export KUBECTL_VSPHERE_PASSWORD="Admin!23"
+
+kubectl vsphere login --server=$server --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify
+kubectl config use-context $ns
+
+#Patch PSP
+kubectl vsphere login --server=$server --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace=$ns --tanzu-kubernetes-cluster-name=tkgs-cluster-$number
+cat << EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: auth
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: rb-all-sa_ns-auth
+  namespace: auth
+roleRef:
+  kind: ClusterRole
+  name: psp:vmware-system-privileged
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+- kind: Group
+  apiGroup: rbac.authorization.k8s.io
+  name: system:serviceaccounts:auth
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: cert-manager
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: rb-all-sa_ns-cert-manager
+  namespace: cert-manager
+roleRef:
+  kind: ClusterRole
+  name: psp:vmware-system-privileged
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+- kind: Group
+  apiGroup: rbac.authorization.k8s.io
+  name: system:serviceaccounts:cert-manager
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: istio-system
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: rb-all-sa_ns-istio-system
+  namespace: istio-system
+roleRef:
+  kind: ClusterRole
+  name: psp:vmware-system-privileged
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+- kind: Group
+  apiGroup: rbac.authorization.k8s.io
+  name: system:serviceaccounts:istio-system
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: knative-serving
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: rb-all-sa_ns-knative-serving
+  namespace: knative-serving
+roleRef:
+  kind: ClusterRole
+  name: psp:vmware-system-privileged
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+- kind: Group
+  apiGroup: rbac.authorization.k8s.io
+  name: system:serviceaccounts:knative-serving
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: kubeflow
+  labels:
+    control-plane: kubeflow
+    istio-injection: enabled
+---
+kind: RoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: rb-all-sa_ns-kubeflow
+  namespace: kubeflow
+roleRef:
+  kind: ClusterRole
+  name: psp:vmware-system-privileged
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+- kind: Group
+  apiGroup: rbac.authorization.k8s.io
+  name: system:serviceaccounts:kubeflow
+EOF
+echo "Patch PSP -- done"
+
+cd manifests-1.4-branch
+while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+echo "deploy kubeflow -- done"
+
+exit
--- tkg-cluster-deployer-zyj.sh
+++ tkg-cluster-deployer-zyj.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-number=23
+number=24
 ns=zyajing
 server=10.117.233.2
 
