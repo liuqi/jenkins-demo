ba4b54ffe871604c7c85852e602a401ae17979bd
zyajing <zyajing@vmware.com>
2022-02-24 21:59:33 -0800

    Update kubeflow-deployer.sh file

--- kubeflow-deployer.sh
+++ kubeflow-deployer.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-number=20
+number=22
 ns=zyajing
 server=10.117.233.2
 
 export PATH=./00-kubectl-vsphere-plugin/bin:$PATH
 export KUBECTL_VSPHERE_PASSWORD="Admin!23"
 
-kubectl vsphere login --server=$server --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify
-kubectl config use-context $ns
-
-#Patch PSP
+#Patch PSP --- robelBinding
 kubectl vsphere login --server=$server --vsphere-username administrator@vsphere.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-namespace=$ns --tanzu-kubernetes-cluster-name=tkgs-cluster-$number
 cat << EOF | kubectl apply -f -
 apiVersion: v1
@@ -114,6 +111,26 @@ EOF
 echo "Patch PSP -- done"
 
 while ! kustomize build ./manifests-1.4-branch/example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+
+while true; do
+  kubectl get pods -A |grep Running
+  if [[ $? == 0 ]]; then
+    break
+  fi
+  sleep 10
+  echo "Wait Pods Running..."
+done
+
+sleep 200
+
+while true; do
+  kubectl get ns|grep kubeflow-user-example-com
+  if [[ $? == 0 ]]; then
+    break
+  fi
+  sleep 10
+  echo "Wait kubeflow-user-example-com namespace creating finish..."
+done
 cat << EOF | kubectl apply -f -
 kind: RoleBinding
 apiVersion: rbac.authorization.k8s.io/v1
@@ -129,7 +146,6 @@ subjects:
   apiGroup: rbac.authorization.k8s.io
   name: system:serviceaccounts:kubeflow-user-example-com
 EOF
-
 echo "deploy kubeflow -- done"
 
 exit
